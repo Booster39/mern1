99e12d17aaa77abab184888a9baad15337fa17a6
Abdoulaye DOUCOURE <abdoulaye.doucoure@epitech.eu>
2021-12-12 23:27:35 +0100

    [ADD] main folders and options

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                Navbar
+                Header
+            </div>
+            Brand
+            WhatGPT3
+            Features
+            Possibility
+            Cta
+            Blog
+            Footer
+        </div>
+    )
+}
+
+export default App
